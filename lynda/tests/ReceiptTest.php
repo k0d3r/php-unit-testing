db7da825f8cbcbdec0e4d9366187a662a1cfe5b3
k0d3r <k0d3r@ayarasolutions.com>
2019-03-15 20:22:12 +0000

    Change indentation to spaces

--- lynda/tests/ReceiptTest.php
+++ lynda/tests/ReceiptTest.php
@@ -8,107 +8,107 @@ use TDD\Receipt;
 
 class ReceiptTest extends TestCase
 {
-	private $receipt;
+    private $receipt;
 
-	public function setUp()
-	{
-		$this->receipt = new Receipt;
-	}
+    public function setUp()
+    {
+        $this->receipt = new Receipt;
+    }
 
-	public function tearDown()
-	{
-		unset($this->receipt);
-	}
+    public function tearDown()
+    {
+        unset($this->receipt);
+    }
 
-	// Tests total()
-	public function testTotal()
-	{
-		$input = [0,2,5,8];
-		$expected = 15;
-		$output = $this->receipt->total($input, null);
-		$this->assertEquals($expected, $output, sprintf('Failure message from: %s', __METHOD__));
-	}
+    // Tests total()
+    public function testTotal()
+    {
+        $input = [0,2,5,8];
+        $expected = 15;
+        $output = $this->receipt->total($input, null);
+        $this->assertEquals($expected, $output, sprintf('Failure message from: %s', __METHOD__));
+    }
 
-	// Tests total()
-	public function testTotalAndCoupon()
-	{
-		$input = [0,2,5,8];
-		$coupon = 0.20;
-		$expected = 12;
-		$output = $this->receipt->total($input, $coupon);
-		$this->assertEquals($expected, $output, sprintf('Failure message from: %s', __METHOD__));
-	}
+    // Tests total()
+    public function testTotalAndCoupon()
+    {
+        $input = [0,2,5,8];
+        $coupon = 0.20;
+        $expected = 12;
+        $output = $this->receipt->total($input, $coupon);
+        $this->assertEquals($expected, $output, sprintf('Failure message from: %s', __METHOD__));
+    }
 
-	// Tests total()
-	public function testTotalException()
-	{
-		$input = [0,2,5,8];
-		$coupon = 1.20;
-		$this->expectException('BadMethodCallException');
-		$this->receipt->total($input, $coupon);
-	}
+    // Tests total()
+    public function testTotalException()
+    {
+        $input = [0,2,5,8];
+        $coupon = 1.20;
+        $this->expectException('BadMethodCallException');
+        $this->receipt->total($input, $coupon);
+    }
 
-	public function testTax()
-	{
-		$input = 10.00;
-		$tax = 0.10;
-		$expected = 1.00;
-		$output = $this->receipt->tax($input, $tax);
-		$this->assertEquals($expected, $output, sprintf('Failure message from: %s', __METHOD__));
-	}
+    public function testTax()
+    {
+        $input = 10.00;
+        $tax = 0.10;
+        $expected = 1.00;
+        $output = $this->receipt->tax($input, $tax);
+        $this->assertEquals($expected, $output, sprintf('Failure message from: %s', __METHOD__));
+    }
 
-	public function testPostTaxTotal()
-	{
-		$items = ['MOCKED_VALUES'];
-		$tax = 0.20;
-		$coupon = null;
+    public function testPostTaxTotal()
+    {
+        $items = ['MOCKED_VALUES'];
+        $tax = 0.20;
+        $coupon = null;
 
-		$receipt = $this->getMockBuilder('TDD\Receipt')
-			->setMethods(['total', 'tax']) // Methods that the mock object will respond to
-			->getMock();
+        $receipt = $this->getMockBuilder('TDD\Receipt')
+            ->setMethods(['total', 'tax']) // Methods that the mock object will respond to
+            ->getMock();
 
-		$receipt
-			->expects($this->exactly(1)) // We only expect to call the total() method this amount of times
-			->method('total')
-			->with($items, $coupon) // with() turns the stub into a mock. Tesing with params is prob not necessary
-			->will($this->returnValue(10)); // The $subTotal var must be the same as tyhis if reused further in the code
+        $receipt
+            ->expects($this->exactly(1)) // We only expect to call the total() method this amount of times
+            ->method('total')
+            ->with($items, $coupon) // with() turns the stub into a mock. Tesing with params is prob not necessary
+            ->will($this->returnValue(10)); // The $subTotal var must be the same as tyhis if reused further in the code
 
-		$receipt
-			->expects($this->once())
-			->method('tax')
-			->with(10, $tax) // The first param passed here must be equal to the return value of total()
-			->will($this->returnValue(1.00));
+        $receipt
+            ->expects($this->once())
+            ->method('tax')
+            ->with(10, $tax) // The first param passed here must be equal to the return value of total()
+            ->will($this->returnValue(1.00));
 
-		// Items, tax & coupon code arguments.
-		// All these values do not matter because the return values from total() and tax() are already set
-		$result = $receipt->postTaxTotal($items, $tax, $coupon);
-		$expectedResult = 11;
-		$this->assertEquals($expectedResult, $result);
-	}
+        // Items, tax & coupon code arguments.
+        // All these values do not matter because the return values from total() and tax() are already set
+        $result = $receipt->postTaxTotal($items, $tax, $coupon);
+        $expectedResult = 11;
+        $this->assertEquals($expectedResult, $result);
+    }
 
-	/**
-	 * Data Providers enable testing of multiple input variables for a method
-	 * @dataProvider addProvider
-	 */
-	public function testAdd($input, $expected)
-	{
-		$expectedResult = $expected;
-		$result = $this->receipt->add($input);
-		
-		$this->assertEquals($expectedResult, $result, "Sum should equal $expected");
-	}
-	public function addProvider()
-	{
-		// Array elements match up to function params. The test function can take in any args, not related to the function to test method signature
-		return [
-			[
-				[1,2,3],
-				6
-			],
-			[
-				[-1,2,3],
-				4
-			]
-		];
-	}
+    /**
+     * Data Providers enable testing of multiple input variables for a method
+     * @dataProvider addProvider
+     */
+    public function testAdd($input, $expected)
+    {
+        $expectedResult = $expected;
+        $result = $this->receipt->add($input);
+        
+        $this->assertEquals($expectedResult, $result, "Sum should equal $expected");
+    }
+    public function addProvider()
+    {
+        // Array elements match up to function params. The test function can take in any args, not related to the function to test method signature
+        return [
+            [
+                [1,2,3],
+                6
+            ],
+            [
+                [-1,2,3],
+                4
+            ]
+        ];
+    }
 }
